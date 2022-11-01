449470ab1788c53a11bdf1ce459012cf43dab3c6
Arequitae <1471206738@qq.com>
2022-11-01 17:21:56 +0800

    feat: change script position

--- plugin.ts
+++ plugin.ts
@@ -4,9 +4,12 @@ import { IApi } from 'umi';
 
 export default (api: IApi) => {
   api.modifyHTML(($) => {
-    $('body').append([
-      '<script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=YejPRoMWOl5OUBKZsCd1pA2p00jHEvXirjU1HBPkY_o"></script>',
-    ]);
+    // eslint-disable-next-line no-sparse-arrays
+    // $('body').append([
+    //   '<script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=YejPRoMWOl5OUBKZsCd1pA2p00jHEvXirjU1HBPkY_o"></script>',
+    //   ,
+    //   '<script type="text/javascript" id="clustrmaps" src="//clustrmaps.com/map_v2.js?d=YejPRoMWOl5OUBKZsCd1pA2p00jHEvXirjU1HBPkY_o&cl=ffffff&w=a"></script>'
+    // ]);
     return $;
   });
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,21 +2,26 @@ import FunctionCal from '@/components/FunctionCal';
 import { PageContainer } from '@ant-design/pro-components';
 import styles from './index.less';
 import { testConnect } from '../../services/basicService';
+import { useEffect } from 'react';
 
 const HomePage = () => {
   testConnect().then((data) => {
     console.log(data);
   });
+  useEffect(() => {
+    let script2 = document.createElement('script');
+    script2.id = 'clustrmaps';
+    script2.type = 'text/javascript';
+    script2.src =
+      '//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=500&t=tt&d=YejPRoMWOl5OUBKZsCd1pA2p00jHEvXirjU1HBPkY_o&co=2d78ad&cmo=3acc3a&cmn=ff5353&ct=ffffff';
+    document.querySelector('.' + styles.globalMap).appendChild(script2);
+  }, []);
   return (
     <PageContainer ghost>
       <div className={styles.container}>
         <h1>Arequitae</h1>
-        <script
-          type="text/javascript"
-          id="clstr_globe"
-          src="//clustrmaps.com/globe.js?d=YejPRoMWOl5OUBKZsCd1pA2p00jHEvXirjU1HBPkY_o"
-        ></script>
       </div>
+      <div className={styles.globalMap}></div>
     </PageContainer>
   );
 };
